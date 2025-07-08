5f9fc6c26e3ef4254a740f149744aff5cbad39b9
Cauã Pereira <cauapereira_@hotmail.com>
2025-07-08 20:20:22 -0300

    feat: create CssComponents and Home components

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import * as Styled from "./CssComponents";
+
+function Home() {
+  return (
+    <div>
+      <Styled.Title>Welcome!</Styled.Title>;
+      <p>This is the main page of our application.</p>
+    </div>
+  );
+}
+
+export default Home;
