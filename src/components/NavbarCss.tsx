ced0a007bb009bac6550ae99ecbedfb2a505604c
Cauã Pereira <cauapereira_@hotmail.com>
2025-07-09 20:25:56 -0300

    feat: add NavbarCss component for navigation styling; update global styles in index.css

---
+++ src/components/NavbarCss.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const NavbarContainer = styled.nav`
+  display: flex;
+  margin-top: 40px;
+`;
+
+export const NavbarList = styled.ul`
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  margin-left: 30px;
+`;
+
+export const NavbarItem = styled.li`
+  color: white;
+  font-size: 34px;
+  font-family: "Inter", sans-serif;
+  margin: 38px;
+`;
--- src/pages/home/CssComponents.tsx
+++ src/pages/home/CssComponents.tsx
@@ -4,3 +4,11 @@ export const Title = styled.h1`
   font-size: 2em;
   color: #cecece;
 `;
+
+export const Introduce = styled.p`
+  font-size: 38px;
+  color: white;
+  font-family: "Inter", sans-serif;
+  font-weight: 100;
+  margin-left: 68px;
+`;
