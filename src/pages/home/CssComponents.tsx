14adf39c11f79366754c564518a19ad556ef1c4e
Cauã Pereira <cauapereira_@hotmail.com>
2025-07-10 21:03:46 -0300

    feat: update Home component introduction and presentation sections

--- src/pages/home/CssComponents.tsx
+++ src/pages/home/CssComponents.tsx
@@ -11,4 +11,46 @@ export const Introduce = styled.p`
   font-family: "Inter", sans-serif;
   font-weight: 100;
   margin-left: 68px;
+  margin-top: 80px;
+`;
+
+export const ButtonSection = styled.div`
+  width: 50%;
+  margin-left: 48px;
+  margin-top: 70px;
+  display: flex;
+`;
+
+export const Button = styled.button`
+  background-color: #0039bc;
+
+  text-align: center;
+  font-size: 24px;
+  font-family: "Inter", sans-serif;
+  font-weight: 400;
+
+  border-radius: 8px;
+  border: 0;
+  outline: none;
+  width: 267px;
+
+  height: 64px;
+  margin-left: 20px;
+  color: white;
+  cursor: pointer;
+
+  @media (hover: hover) and (pointer: fine) {
+    &:hover {
+      background-color: #002a8c;
+    }
+  }
+`;
+
+export const Presentation = styled.p`
+  font-size: 21px;
+  color: white;
+  font-family: "Inter", sans-serif;
+  font-weight: 100;
+  margin-left: 68px;
+  margin-top: 15px;
 `;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,9 +6,18 @@ function Home() {
     <div>
       <Navbar />
       <Styled.Introduce>
-        Hi,
-        <br /> I’m Cauã Pereira, and Software Developer
+        Welcome!
+        <br /> I’m Cauã Pereira, a Software Developer
       </Styled.Introduce>
+      <Styled.Presentation>
+        I'm passionate about creating complete and efficient <br />
+        software solutions. Here, you can explore some of my projects <br /> and
+        get a sense of who I am as a professional.
+      </Styled.Presentation>
+      <Styled.ButtonSection>
+        <Styled.Button>See projects</Styled.Button>
+        <Styled.Button>Contact me</Styled.Button>
+      </Styled.ButtonSection>
     </div>
   );
 }
