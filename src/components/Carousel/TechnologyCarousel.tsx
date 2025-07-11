3ca311ac25f758fa392152c9d84e1fa047f79fe9
Cauã Pereira <cauapereira_@hotmail.com>
2025-07-11 20:25:57 -0300

    feat: refactor TechnologyCarousel to use state for dynamic technology display

--- src/components/Carousel/TechnologyCarousel.tsx
+++ src/components/Carousel/TechnologyCarousel.tsx
@@ -5,18 +5,41 @@ import { DiDocker } from "react-icons/di";
 import { SiSequelize } from "react-icons/si";
 import { FaAws } from "react-icons/fa";
 import * as Styled from "./CssComponents";
+import { useState, type JSX } from "react";
+
+type Technology = {
+  name: string;
+  icon: JSX.Element;
+};
+
+const listOfTechnologies = [
+  { name: "React", icon: <FaReact size={100} /> },
+  { name: "Node.js", icon: <FaNode size={100} /> },
+  { name: "Java", icon: <FaJava size={100} /> },
+  { name: "Docker", icon: <DiDocker size={100} /> },
+  { name: "Sequelize", icon: <SiSequelize size={100} /> },
+  { name: "AWS", icon: <FaAws size={100} /> },
+];
 
 function TechnologyCarousel() {
+  const [technologies, setTechnologies] =
+    useState<Technology[]>(listOfTechnologies);
+
+  const scrollArray = () => {
+    const newList = [...technologies];
+    const firstElement = newList.shift();
+    if (firstElement) newList.push(firstElement);
+    setTechnologies(newList);
+  };
+
   return (
     <div>
       <Styled.CarouselContainer>
-        <FaReact size={100} />
-        <FaNode size={100} />
-        <FaJava size={100} />
-        <DiDocker size={100} />
-        <SiSequelize size={100} />
-        <FaAws size={100} />
+        {technologies.map((tech, index) => (
+          <div key={index}>{tech.icon}</div>
+        ))}
       </Styled.CarouselContainer>
+      <button onClick={scrollArray}>Next list item</button>
     </div>
   );
 }
