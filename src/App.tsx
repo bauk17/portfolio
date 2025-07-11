36013ada28f69f0b00b2f4e789c2c1ce9722fbf1
Cauã Pereira <cauapereira_@hotmail.com>
2025-07-10 21:03:10 -0300

    feat: update App component to render Home and TechnologyCarousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import Navbar from "./components/Navbar";
+import TechnologyCarousel from "./components/Carousel/TechnologyCarousel";
 import Home from "./pages/home/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <Home />
+      <TechnologyCarousel />
+    </div>
+  );
 }
 
 export default App;
