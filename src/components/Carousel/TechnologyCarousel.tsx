6ece21c5c838bb071c331864d40181b4844c6609
Cauã Pereira <cauapereira_@hotmail.com>
2025-07-18 18:43:18 -0300

    feat: add JavaScript and TypeScript icons to TechnologyCarousel and improve autoplay functionality

--- src/components/Carousel/TechnologyCarousel.tsx
+++ src/components/Carousel/TechnologyCarousel.tsx
@@ -1,7 +1,9 @@
 import { FaReact, FaJava, FaAws } from "react-icons/fa";
+import { TbBrandTypescript } from "react-icons/tb";
 import { FaNode } from "react-icons/fa6";
 import { DiDocker } from "react-icons/di";
 import { SiMysql, SiSequelize } from "react-icons/si";
+import { RiJavascriptLine } from "react-icons/ri";
 import { DiPostgresql, DiMongodb } from "react-icons/di";
 import { MdNavigateNext } from "react-icons/md";
 import * as Styled from "./CssComponents";
@@ -22,6 +24,8 @@ const listOfTechnologies = [
   { name: "MySQL", icon: <SiMysql size={100} /> },
   { name: "PostgreSQL", icon: <DiPostgresql size={100} /> },
   { name: "MongoDB", icon: <DiMongodb size={100} /> },
+  { name: "JavaScript", icon: <RiJavascriptLine size={100} /> },
+  { name: "TypeScript", icon: <TbBrandTypescript size={100} /> },
 
   { name: "React", icon: <FaReact size={100} /> },
   { name: "Node.js", icon: <FaNode size={100} /> },
@@ -32,11 +36,14 @@ const listOfTechnologies = [
   { name: "MySQL", icon: <SiMysql size={100} /> },
   { name: "PostgreSQL", icon: <DiPostgresql size={100} /> },
   { name: "MongoDB", icon: <DiMongodb size={100} /> },
+  { name: "JavaScript", icon: <RiJavascriptLine size={100} /> },
+  { name: "TypeScript", icon: <TbBrandTypescript size={100} /> },
 ];
 
 function TechnologyCarousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [animate, setAnimate] = useState(true);
+  const [autoPlay, setAutoPLay] = useState(true);
 
   const visibleCount = listOfTechnologies.length / 2;
 
@@ -46,9 +53,10 @@ function TechnologyCarousel() {
   };
 
   useEffect(() => {
+    if (!autoPlay) return;
     const interval = setInterval(scrollArray, 2000);
     return () => clearInterval(interval);
-  }, []);
+  }, [autoPlay]);
 
   useEffect(() => {
     if (currentIndex === visibleCount) {
@@ -61,7 +69,10 @@ function TechnologyCarousel() {
 
   return (
     <div>
-      <Styled.CarouselWrapper>
+      <Styled.CarouselWrapper
+        onMouseEnter={() => setAutoPLay(false)}
+        onMouseLeave={() => setAutoPLay(true)}
+      >
         <Styled.CarouselContainer
           animate={animate}
           style={{ transform: `translateX(-${currentIndex * 140}px)` }}
@@ -71,7 +82,11 @@ function TechnologyCarousel() {
           ))}
         </Styled.CarouselContainer>
       </Styled.CarouselWrapper>
-      <Styled.CarouselNextButton onClick={scrollArray}>
+      <Styled.CarouselNextButton
+        onClick={scrollArray}
+        onMouseEnter={() => setAutoPLay(false)}
+        onMouseLeave={() => setAutoPLay(true)}
+      >
         <MdNavigateNext color="white" size={25} />
       </Styled.CarouselNextButton>
     </div>
