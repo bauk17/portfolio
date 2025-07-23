83f10d63d5f3f0e5bcaeec49cfb08f614a851eb8
Cauã Pereira <cauapereira_@hotmail.com>
2025-07-22 21:39:56 -0300

    feat: add TechDescriptionContainer and TechDescriptionText styled components for technology descriptions

---
+++ src/components/Carousel/StyledTechDescription.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const TechDescriptionContainer = styled.div`
+  position: absolute;
+  top: 450px;
+  left: 640px;
+  z-index: 10;
+  width: 300px;
+  height: 180px;
+  border-radius: 7px;
+  background-color: #2a4072;
+`;
+
+export const TechDescriptionText = styled.p`
+  color: #ffffff;
+  font-size: 15px;
+  font-family: "Inter", sans-serif;
+`;
