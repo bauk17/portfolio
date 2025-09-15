af3b66e51d42077b0972724caf372eef3dc4378b
Cauã Pereira <cauapereira_@hotmail.com>
2025-09-14 23:54:22 -0300

    feat: create About page and associated styles for personal introduction

---
+++ src/pages/about/About.tsx
@@ -0,0 +1,21 @@
+import * as Styled from "./AboutCss";
+
+function About() {
+  return (
+    <div>
+      <Styled.AboutContainer>
+        My name is Cauã Pereira, a 21-year-old Software Developer currently
+        pursuing a degree in Computer Science. I had my first contact with
+        programming at the age of 17, and I immediately fell in love with the
+        endless possibilities of creating solutions through code. Since then,
+        I’ve been continuously learning and building projects to strengthen my
+        skills. I am passionate about solving problems and using technology to
+        create meaningful solutions that can make a difference in people’s
+        lives. Ambition drives me to grow every day, not only as a professional
+        but also as a person, always striving to improve and deliver my best.
+      </Styled.AboutContainer>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/about/AboutCss.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const AboutContainer = styled.div`
+  margin-left: 68px;
+  margin-top: 80px;
+  width: 800px;
+  font-family: "Inter", sans-serif;
+  font-size: 18px;
+  font-weight: 100;
+`;
