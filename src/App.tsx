8096343496deb7aeabe62eb2591767514db840f6
Cauã Pereira <cauapereira_@hotmail.com>
2025-09-14 23:55:10 -0300

    feat: update App component to include Navbar and MainContent with navigation functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { MdNavigateNext } from "react-icons/md";
 import TechnologyCarousel from "./components/Carousel/TechnologyCarousel";
-import Home from "./pages/home/Home";
+import MainContent from "./components/mainContent/MainContent";
+import * as Styled from "./AppCss";
+
+import Navbar from "./components/Navbar";
+import { useState } from "react";
 
 function App() {
+  const pages = ["Home", "About", "Projects", "Contact"];
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <div>
-      <Home />
+      <Navbar
+        pages={pages}
+        activePage={pages[activeIndex]}
+        setActivePage={(page) => setActiveIndex(pages.indexOf(page))}
+      />
+      <MainContent activePage={pages[activeIndex]} />
       <TechnologyCarousel />
+      <Styled.NextPageButton
+        onClick={() => {
+          setActiveIndex((prev) => (prev + 1) % pages.length);
+        }}
+      >
+        <MdNavigateNext color="white" size={25} />
+      </Styled.NextPageButton>
     </div>
   );
 }
