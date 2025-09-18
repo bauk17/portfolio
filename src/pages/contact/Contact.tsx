7de4008b51f9368d6856317a633267e03aba76e3
Cauã Pereira <cauapereira_@hotmail.com>
2025-09-18 07:40:51 -0300

    feat: update Contact component to include contact information and links

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,7 +1,10 @@
 function Contact() {
   return (
     <div>
-      <div></div>
+      <div>
+        You can contact me using the following links:{" "}
+        <a href="https://github.com/bauk17/">github</a>
+      </div>
     </div>
   );
 }
