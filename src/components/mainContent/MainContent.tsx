c806092b9c51c75ee912fc507dcc7c7dc2e09277
Cauã Pereira <cauapereira_@hotmail.com>
2025-11-21 20:27:23 -0300

    feat: refactor MainContent component to use a mapping for page rendering

--- src/components/mainContent/MainContent.tsx
+++ src/components/mainContent/MainContent.tsx
@@ -1,20 +1,45 @@
 import About from "../../pages/about/About";
+import * as MainContentStyled from "./MainContentCss";
 import Contact from "../../pages/contact/Contact";
 import Home from "../../pages/home/Home";
 import Projects from "../../pages/projects/Projects";
+import { useEffect, useState } from "react";
 
-interface Props {
+type PageName = "Home" | "About" | "Projects" | "Contact";
+
+const PageComponentMap: Record<PageName, React.FC> = {
+  Home: Home,
+  About: About,
+  Projects: Projects,
+  Contact: Contact,
+};
+
+interface MainContentProps {
   activePage: string;
 }
 
-function MainContent({ activePage }: Props) {
+function MainContent({ activePage }: MainContentProps) {
+  const pages = ["Home", "About", "Projects", "Contact"];
+
+  const activeIndex = pages.indexOf(activePage);
+
+  const transformOffset = `translateX(-${activeIndex * 100}%)`;
+
   return (
-    <div>
-      {activePage === "Home" && <Home />}
-      {activePage === "About" && <About />}
-      {activePage === "Projects" && <Projects />}
-      {activePage === "Contact" && <Contact />}
-    </div>
+    <MainContentStyled.default.ContentWrapper>
+      <MainContentStyled.default.ContentTrack transformOffset={transformOffset}>
+        {pages.map((pageName) => {
+          const Component = PageComponentMap[pageName as PageName];
+          if (!Component) return null;
+
+          return (
+            <MainContentStyled.default.PageContent key={pageName}>
+              <Component />
+            </MainContentStyled.default.PageContent>
+          );
+        })}
+      </MainContentStyled.default.ContentTrack>
+    </MainContentStyled.default.ContentWrapper>
   );
 }
 
