b5c4b55f5770932262f0f8d58174d5c2f8e4994a
Cauã Pereira <cauapereira_@hotmail.com>
2025-11-21 20:27:54 -0300

    feat: add styled components for timeline navigation in About section

--- src/pages/about/AboutCss.tsx
+++ src/pages/about/AboutCss.tsx
@@ -1,10 +1,25 @@
 import styled from "styled-components";
 
 export const AboutContainer = styled.div`
+  /* -> name: TimelineAbout*/
   margin-left: 68px;
   margin-top: 80px;
   width: 800px;
   font-family: "Inter", sans-serif;
   font-size: 18px;
   font-weight: 100;
+`;
+
+export const TimelineNav = styled.nav`
+  background-color: red;
+  margin-left: 68px;
+`;
+
+export const TimelineUl = styled.ul`
+  list-style: none;
+  display: flex;
+`;
+
+export const TimelineItem = styled.li`
+  margin-left: 20px;
 `;
