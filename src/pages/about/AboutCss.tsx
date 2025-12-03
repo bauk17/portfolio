17e8b85478fe39b6d8fc8f0ba56aae335d6aa9d9
Cauã Pereira <cauapereira_@hotmail.com>
2025-12-03 10:13:59 -0300

    feat: add Timeline component with interactive progress tracking

--- src/pages/about/AboutCss.tsx
+++ src/pages/about/AboutCss.tsx
@@ -1,25 +1,70 @@
 import styled from "styled-components";
 
 export const AboutContainer = styled.div`
-  /* -> name: TimelineAbout*/
   margin-left: 68px;
   margin-top: 80px;
   width: 800px;
   font-family: "Inter", sans-serif;
   font-size: 18px;
-  font-weight: 100;
+  font-weight: 200;
 `;
 
-export const TimelineNav = styled.nav`
-  background-color: red;
-  margin-left: 68px;
+export const AboutLetter = styled.div`
+  margin-top: 50px;
 `;
 
-export const TimelineUl = styled.ul`
-  list-style: none;
+export const AboutContent = styled.div`
+  margin-left: 64px;
+  margin-top: 150px;
+`;
+
+export const TimelineWrapper = styled.div`
+  margin-top: 30px;
+  margin-left: 98px;
+  width: 800px;
+  position: relative;
   display: flex;
+  justify-content: space-between;
 `;
 
-export const TimelineItem = styled.li`
-  margin-left: 20px;
+export const TimeLineContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+`;
+
+export const TimeLineLabel = styled.div`
+  font-family: "Inter", sans-serif;
+  font-size: 14.5px;
+`;
+
+export const TimelineButton = styled.button<{
+  active?: boolean;
+  isDone?: boolean;
+}>`
+  background-color: ${({ active, isDone }) =>
+    isDone ? "#17a005" : active ? "#0039bc" : "#cfcfcf"};
+  width: 20px;
+  height: 20px;
+  margin-top: 15px;
+  border-radius: 100%;
+  cursor: pointer;
+  border: none;
+`;
+
+export const TimeLineProgressBar = styled.div<{
+  progress: number;
+  isDone: boolean;
+}>`
+  position: absolute;
+  top: 40.5px;
+  left: 45px;
+
+  height: 4px;
+
+  background-color: ${({ isDone }) => (isDone ? "#17a005" : "#0039bc")};
+  width: calc(${({ progress }) => progress}% - 21px);
+  transition: width 0.4s ease;
+  z-index: 0;
 `;
