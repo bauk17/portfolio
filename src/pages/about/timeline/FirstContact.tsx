ac1eb013a5a574afad61fc351e1c9b5996c4ba13
Cauã Pereira <cauapereira_@hotmail.com>
2025-12-03 10:14:47 -0300

    refactor: remove About component and its timeline subcomponents

--- src/pages/about/timeline/FirstContact.tsx
+++
@@ -1,9 +0,0 @@
-function FirstContact() {
-  return (
-    <div>
-      <div>I had my first contact when i was...</div>
-    </div>
-  );
-}
-
-export default FirstContact;
--- src/pages/about/timeline/FirstProjects.tsx
+++
@@ -1,9 +0,0 @@
-function FirstProjects() {
-  return (
-    <div>
-      <div>I built a website using...</div>
-    </div>
-  );
-}
-
-export default FirstProjects;
