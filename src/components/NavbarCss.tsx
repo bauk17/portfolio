db2bd50e58c2f3e7e832c98130e1650d5bb1356a
Cauã Pereira <cauapereira_@hotmail.com>
2025-12-04 21:20:45 -0300

    refactor: improve Navbar component styling for better responsiveness

--- src/components/NavbarCss.tsx
+++ src/components/NavbarCss.tsx
@@ -2,15 +2,28 @@ import styled from "styled-components";
 
 export const NavbarContainer = styled.nav`
   display: flex;
+  justify-content: center;
   margin-top: 70px;
+
+  width: 100%;
+
+  @media (max-width: 500px) {
+    margin-top: 50px;
+    margin-left: 30px;
+  }
 `;
 
 export const NavbarList = styled.ul`
   display: flex;
+  justify-content: center;
+
   list-style: none;
   padding: 0;
   margin: 0;
   margin-left: 30px;
+  @media (max-width: 500px) {
+    margin-left: 4px;
+  }
 `;
 
 export const NavbarItem = styled.li<{ $active?: boolean }>`
@@ -23,5 +36,10 @@ export const NavbarItem = styled.li<{ $active?: boolean }>`
   &:hover {
     opacity: 0.5;
     transition: 0.5s all ease;
+  }
+
+  @media (max-width: 500px) {
+    font-size: 19px;
+    margin: 7px;
   }
 `;
